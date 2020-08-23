3356884e0b86cfeb3d6c8d3a6444c0cfeba69fb3
hardirichard <hardi.richard@gmail.com>
2020-08-23 20:59:20 +0200

    Initial Commit

---
+++ test_exercise_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_exercise_1
+//
+//  Created by Richárd Hardi on 2020. 08. 23..
+//  Copyright © 2020. Richárd Hardi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
